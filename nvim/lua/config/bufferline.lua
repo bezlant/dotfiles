c6149fee595b04c7dd2ac8ab6f688498414e484b
bezlant <abezludniy@mail.ru>
2022-07-24 12:51:11 +0300

    Improved look of NvimTree & alacritty autocommands added.

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -1,25 +1,25 @@
-local status_ok, bufferline = pcall(require, "bufferline") if not status_ok then
-    vim.notify("Can't load bufferline :(")
-    return
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+	vim.notify("Can't load bufferline :(")
+	return
 end
 
-bufferline.setup {
-    options = {
-        themable = true,
-        offsets = {{ filetype = "NvimTree", text = "", highlight = "Directory", padding = 1 }},
-        show_close_icon = false,
-        show_buffer_close_icons = false,
-        modified_icon = "*",
-        tab_size = 15,  
-        max_name_length = 18,
-        always_show_bufferline = false,
-        separator_style = "thin",
-    },
+bufferline.setup({
+	options = {
+		themable = true,
+		close_command = "Bdelete! %d",
+		show_close_icon = false,
+		show_buffer_close_icons = false,
+		modified_icon = "*",
+		tab_size = 15,
+		max_name_length = 18,
+		always_show_bufferline = false,
+		separator_style = "thin",
+	},
 
-    highlights = {
-        buffer_selected = {
-             gui = "bold",
-        },
-    },
-}
-
+	highlights = {
+		buffer_selected = {
+			gui = "bold",
+		},
+	},
+})
--- nvim/lua/config/nvimtree.lua
+++ nvim/lua/config/nvimtree.lua
@@ -1,55 +1,99 @@
 local status_ok, nvimtree = pcall(require, "nvim-tree")
 if not status_ok then
-    vim.notify("Can't load nvim-tree")
+	vim.notify("Can't load nvim-tree")
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-    vim.notify("Can't load nvim-tree-config")
-    return
+	vim.notify("Can't load nvim-tree-config")
+	return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvimtree.setup({
-    auto_reload_on_write = true,
+	auto_reload_on_write = true,
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_cursor = true,
+	sync_root_with_cwd = true,
 
-    view = {
-        adaptive_size = true,
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = "v", cb = tree_cb "vsplit" },
-            },
-        },
-    },
+	view = {
+		adaptive_size = true,
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = "v", cb = tree_cb("vsplit") },
+				{ key = "s", cb = tree_cb("split") },
+			},
+		},
+	},
 
-    renderer = {
-        icons = {
-            glyphs = {
-                default = "",
-                symlink = "",
-                folder = {
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                },
-                git = {
-                    unstaged = "",
-                    staged = "S",
-                    unmerged = "",
-                    renamed = "➜",
-                    deleted = "",
-                    untracked = "U",
-                    ignored = "◌",
-                },
-            },
-        },
-    },
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
 
-    filters = {
-        dotfiles = true,
-    },
+	update_focused_file = {
+		update_cwd = true,
+	},
+
+	renderer = {
+		add_trailing = false,
+		group_empty = true,
+		highlight_opened_files = "all",
+		root_folder_modifier = ":~",
+
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				none = " ",
+			},
+		},
+
+		icons = {
+			webdev_colors = true,
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+			},
+		},
+		special_files = {
+			"Makefile",
+			"build/",
+			"CMakeLists.txt",
+			"CMakeLists",
+		},
+	},
+
+	filters = {
+		dotfiles = true,
+		custom = {
+			"^\\.git",
+		},
+	},
 })
