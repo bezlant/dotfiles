df3672dfcace94447e95a6fdfc2c7a9780261856
bezlant <abezludniy@mail.ru>
2022-07-28 19:33:37 +0300

    Incremental Selection is OP

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -10,6 +10,15 @@ configs.setup({
 	},
 
 	indent = { enable = true },
-
 	fold = { enable = true },
+
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<CR>",
+			scope_incremental = "<CR>",
+			node_incremental = "<TAB>",
+			node_decremental = "<S-TAB>",
+		},
+	},
 })
