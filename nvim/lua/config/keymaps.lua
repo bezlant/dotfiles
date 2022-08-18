4d5b7dd2b5a39ec89181774cdb81111e77010914
bezlant <abezludniy@mail.ru>
2022-08-18 12:11:22 +0300

    Added keymaps for help & man pages

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -120,6 +120,8 @@ map(
 
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
+map("n", "<leader>fm", "<cmd>Telescope man_pages<cr>", opts)
 
 -- NvimTree
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)
