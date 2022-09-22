eafd49a71e4aba67ecf6d3938ffd95083808fb7a
bezlant <abezludniy@mail.ru>
2022-09-22 09:19:44 +0300

    feat: Rosylyn analazyer enabled & signature help installed

--- nvim/lua/config/lsp/settings/omnisharp.lua
+++ nvim/lua/config/lsp/settings/omnisharp.lua
@@ -1,6 +1,6 @@
 return {
 	organize_imports_on_format = true,
-	enable_roslyn_analyzers = false,
+	enable_roslyn_analyzers = true,
 	analyze_open_documents_only = false,
 	enable_import_completion = true,
 }
