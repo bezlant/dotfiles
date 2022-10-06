c930a3f62434574696ec2f87e6c755510b80029d
bezlant <abezludniy@mail.ru>
2022-10-06 08:55:18 +0300

    fix: Project-nvim gone.

--- nvim/lua/config/nvimtree.lua
+++ nvim/lua/config/nvimtree.lua
@@ -12,7 +12,6 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvimtree.setup({
-	respect_buf_cwd = true,
 	auto_reload_on_write = true,
 	disable_netrw = true,
 	hijack_netrw = true,
@@ -42,9 +41,7 @@ nvimtree.setup({
 	},
 
 	update_focused_file = {
-		enable = true,
 		update_cwd = true,
-		update_root = true,
 	},
 
 	renderer = {
