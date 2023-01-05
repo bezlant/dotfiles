c925b14bce7bce65a1f17b3b9f8fe5b7eb5e8db4
bezlant <abezludniy@mail.ru>
2023-01-05 13:08:50 +0300

    feat: gcc comments out blocks now

--- nvim/lua/config/comment.lua
+++ nvim/lua/config/comment.lua
@@ -4,7 +4,49 @@ if not status_ok then
 	return
 end
 
-comment.setup()
+comment.setup({
+	---Add a space b/w comment and the line
+	padding = true,
+	---Whether the cursor should stay at its position
+	sticky = true,
+	---Lines to be ignored while (un)comment
+	ignore = nil,
+	---LHS of toggle mappings in NORMAL mode
+	toggler = {
+		---Line-comment toggle keymap
+		line = "gbc",
+		---Block-comment toggle keymap
+		block = "gcc",
+	},
+	---LHS of operator-pending mappings in NORMAL and VISUAL mode
+	opleader = {
+		---Line-comment keymap
+		line = "gb",
+		---Block-comment keymap
+		block = "gc",
+	},
+	---LHS of extra mappings
+	extra = {
+		---Add comment on the line above
+		above = "gcO",
+		---Add comment on the line below
+		below = "gco",
+		---Add comment at the end of line
+		eol = "gcA",
+	},
+	---Enable keybindings
+	---NOTE: If given `false` then the plugin won't create any mappings
+	mappings = {
+		---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+		basic = true,
+		---Extra mapping; `gco`, `gcO`, `gcA`
+		extra = true,
+	},
+	---Function to call before (un)comment
+	pre_hook = nil,
+	---Function to call after (un)comment
+	post_hook = nil,
+})
 
 local ft_status_ok, ft = pcall(require, "Comment.ft")
 if not ft_status_ok then
@@ -16,10 +58,10 @@ end
 
 ft
 	-- Set only line comment
-	.set("yaml", "#%s")
+	.set("yaml", { "#%s", "#%s" })
 	-- Or set both line and block commentstring
-	.set("javascript", { "{/*%s*/}" })
-	.set("typescriptreact", { "{/*%s*/}" })
+	.set("javascript", { "{/*%s*/}", "{/*%s*/}" })
+	.set("typescriptreact", { "{/*%s*/}", "{/*%s*/}" })
 
 -- 2. Metatable magic
 
