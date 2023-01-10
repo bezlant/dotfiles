3f10ad6812c075d23501f04e2bd6e2064ab000db
bezlant <abezludniy@mail.ru>
2023-01-10 10:13:10 +0300

    feat: some commets tweaks

--- nvim/lua/config/comment.lua
+++ nvim/lua/config/comment.lua
@@ -4,49 +4,7 @@ if not status_ok then
 	return
 end
 
-comment.setup({
-	---Add a space b/w comment and the line
-	padding = true,
-	---Whether the cursor should stay at its position
-	sticky = true,
-	---Lines to be ignored while (un)comment
-	ignore = nil,
-	---LHS of toggle mappings in NORMAL mode
-	toggler = {
-		---Line-comment toggle keymap
-		line = "gbc",
-		---Block-comment toggle keymap
-		block = "gcc",
-	},
-	---LHS of operator-pending mappings in NORMAL and VISUAL mode
-	opleader = {
-		---Line-comment keymap
-		line = "gb",
-		---Block-comment keymap
-		block = "gc",
-	},
-	---LHS of extra mappings
-	extra = {
-		---Add comment on the line above
-		above = "gcO",
-		---Add comment on the line below
-		below = "gco",
-		---Add comment at the end of line
-		eol = "gcA",
-	},
-	---Enable keybindings
-	---NOTE: If given `false` then the plugin won't create any mappings
-	mappings = {
-		---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-		basic = true,
-		---Extra mapping; `gco`, `gcO`, `gcA`
-		extra = true,
-	},
-	---Function to call before (un)comment
-	pre_hook = nil,
-	---Function to call after (un)comment
-	post_hook = nil,
-})
+comment.setup()
 
 local ft_status_ok, ft = pcall(require, "Comment.ft")
 if not ft_status_ok then
@@ -54,14 +12,6 @@ if not ft_status_ok then
 	return
 end
 
--- 1. Using set function
-
-ft
-	-- Set both line and block commentstring
-	.set("yaml", { "#%s", "#%s" })
-	.set("javascript", { "/*%s*/", "{/*%s*/}" })
+ft.set("javascript", { "/*%s*/", "{/*%s*/}" })
 	.set("typescriptreact", { "/*%s*/", "{/*%s*/}" })
-
--- Multiple filetypes
-ft({ "go", "rust" }, ft.get("c"))
-ft({ "toml", "graphql" }, "#%s")
+	.set("yaml", { "#%s", "#%s" })
