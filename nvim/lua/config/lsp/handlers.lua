f261ff40203a2edc23e8e963f47144e65570cce4
bezlant <abezludniy@mail.ru>
2023-03-08 09:17:30 +0300

    fix: Disabled tsserver formatting

--- nvim/lua/config/lsp/handlers.lua
+++ nvim/lua/config/lsp/handlers.lua
@@ -65,6 +65,10 @@ end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 M.on_attach = function(client, bufnr)
+	if client.name == "tsserver" then
+		client.server_capabilities.documentFormattingProvider = false
+	end
+
 	if client.name ~= "sqls" and client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
