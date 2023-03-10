64b462e27ec371b1fc6c68ca55e6bc1829b97e4c
bezlant <abezludniy@mail.ru>
2023-03-10 07:26:45 +0300

    fix: Tweaking projector settings

--- i3/toggleProjector.sh
+++ i3/toggleProjector.sh
@@ -7,6 +7,8 @@ if xrandr | grep "$extern disconnected"; then
 else
 	xrandr --output "$intern" --off --output "$extern" --auto
 fi
+
 # Turn this into an i3 config script
+# xrandr --output HDMI-A-0 --mode 1920x1080
 # xrandr --output eDP --mode 2560x1600
 # xrandr --output HDMI-A-0 --off
