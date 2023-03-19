acdedda484ea507388209cd6a1cd5e0ff68e94fa
bezlant <abezludniy@mail.ru>
2023-03-19 08:52:19 +0300

    fix: projector doesn't disable current screen

--- i3/toggleProjector.sh
+++ i3/toggleProjector.sh
@@ -5,7 +5,7 @@ extern=HDMI-A-0
 if xrandr | grep "$extern disconnected"; then
 	xrandr --output "$extern" --off --output "$intern" --auto
 else
-	xrandr --output "$intern" --off --output "$extern" --auto
+	xrandr --output "$intern" --output "$extern" --auto
 fi
 
 # Turn this into an i3 config script
